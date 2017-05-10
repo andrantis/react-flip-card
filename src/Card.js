b856c6f253d1f9f28db3321a3ae8c8fe142da058
Andres Antista <andresantista@gmail.com>
2017-05-09 22:40:30 -0700

    click to flip and sizing

--- src/Card.js
+++ src/Card.js
@@ -2,17 +2,31 @@ import React, { Component } from "react";
 import "./Card.css";
 
 export default class Card extends Component {
+  state = {
+    flipped: false
+  };
+
+  handleClick = () => {
+    this.setState({ flipped: !this.state.flipped });
+  };
+
   render() {
     const width = `${this.props.width}px`;
     const height = `${this.props.height}px`;
 
+    const classes = `card-container ${this.state.flipped ? "flipped" : ""}`;
+
     return (
-      <div className="card-container">
-        <div className="card" style={{width: width, height: height}}>
-          <div className="front" style={{width: width, height: height}}>
+      <div
+        className={classes}
+        style={{ width: width, height: height }}
+        onClick={this.handleClick}
+      >
+        <div className="card">
+          <div className="front" style={{ width: width, height: height }}>
             {this.props.front}
           </div>
-          <div className="back" style={{width: width, height: height}}>
+          <div className="back" style={{ width: width, height: height }}>
             {this.props.back}
           </div>
         </div>
